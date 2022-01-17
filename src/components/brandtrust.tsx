8af95c8fb57a90f36223e5f17e432439b457ce47
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-01-17 13:31:56 -0400

    create brands trust parts

---
+++ src/components/brandtrust.tsx
@@ -0,0 +1,16 @@
+//Styles
+import styles from "brandtrust.module.scss";
+
+//Component Brand Trust
+export function Brandtrust() {
+  return (
+    <main>
+      <h1>Marcas que confiam no nosso trabalho</h1>
+      <div className="brands">
+        <a href="#">
+          <img src="" alt="" />
+        </a>
+      </div>
+    </main>
+  );
+}
