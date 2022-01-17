f01c69d438e13479a60d1e67202c266a515c7124
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-01-17 08:19:51 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles/pages/app.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
