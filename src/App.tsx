43036e955630be35bf63c918bfc03c638c8f155f
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-01-17 13:23:16 -0400

    update header, hero and create brands trusts

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Header } from "./components/header";
 import { Hero } from "./components/hero";
 
 //Styles
-import "./styles/app.scss";
+import styles from "./styles/app.module.scss";
 
 //Page App
 export default function App() {
   return (
-    <div className="app">
+    <div className={styles.app}>
       <Header />
       <Hero />
     </div>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,19 +1,23 @@
 //Styles
-import "../styles/hero.scss";
+import styles from "../styles/hero.module.scss";
 
 //Component hero
 export function Hero() {
   return (
     <main>
-      <div className="sideLeft">
-        <h1>Monitoramento Online e Gestão de Ativos em um só lugar</h1>
-        <h3>
-          Evite falhas nas suas máquinas e torne o tempo de inatividade uma
-          coisa do passado com sistema preditivo da TRACTIAN.
-        </h3>
-        <button>Ver mais</button>
+      <div className={styles.sideLeft}>
+        <div>
+          <h1>Monitoramento Online e Gestão de Ativos em um só lugar</h1>
+          <h3>
+            Evite falhas nas suas máquinas e torne o tempo de inatividade uma
+            coisa do passado com sistema preditivo da TRACTIAN.
+          </h3>
+        </div>
+        <button>Saiba Mais</button>
       </div>
-      <div className="sideRight"></div>
+      <div className={styles.sideRight}>
+        <img src="image-hero.svg" alt="" />
+      </div>
     </main>
   );
 }
