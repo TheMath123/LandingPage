506abcda44a884989a1e15b3d20b8439c785e0dd
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-01-18 12:28:26 -0400

    fix bugs

--- src/components/brands.tsx
+++ src/components/brands.tsx
@@ -6,28 +6,24 @@ import styles from "../styles/brands.module.scss";
 //Component Brand Trust
 export function Brands() {
   return (
-    <main className={styles.main}>
-      <h1>Marcas que confiam no nosso trabalho</h1>
-      <div className={styles.brands}>
-        <a href="ahlstrommunksjo">
-          <img src="brands/imgBrand1.svg" alt="ahlstrommunksjo" />
-        </a>
-        <a href="amstedmaxion">
-          <img src="brands/imgBrand2.svg" alt="amstedmaxion" />
-        </a>
-        <a href="corteva">
-          <img src="brands/imgBrand3.svg" alt="corteva" />
-        </a>
-        <a href="embraer">
-          <img src="brands/imgBrand4.svg" alt="embraer" />
-        </a>
-        <a href="master">
-          <img src="brands/imgBrand5.svg" alt="master" />
-        </a>
-        <a href="yara">
-          <img src="brands/imgBrand6.svg" alt="yara" />
-        </a>
-      </div>
-    </main>
+    <div className={styles.app}>
+      <main className={styles.main}>
+        <h1>Marcas que confiam no nosso trabalho</h1>
+        <div className={styles.brands}>
+          <a href="ahlstrommunksjo">
+            <img src="brands/imgBrand1.svg" alt="ahlstrommunksjo" />
+          </a>
+          <a href="amstedmaxion">
+            <img src="brands/imgBrand2.svg" alt="amstedmaxion" />
+          </a>
+          <a href="corteva">
+            <img src="brands/imgBrand3.svg" alt="corteva" />
+          </a>
+          <a href="embraer">
+            <img src="brands/imgBrand4.svg" alt="embraer" />
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }
