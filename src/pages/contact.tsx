9ede393e70021c2fe29599cca12dec5e18801466
Matheus Prado Agostinho <mat.matheus_pa@hotmail.com>
2022-01-19 21:15:09 -0300

    fix design form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,14 +8,16 @@ export function Contact(){
   return(
     <div className={styles.app}>
       <h1>Resolva todas seus problemas de manutenção, possuindo tudo na palma da sua mão.</h1>
-      <form >
-        <span>Fale com a gente</span>
-        <input type="text" placeholder='Nome'/>
-        <input type="email" placeholder='E-mail' />
-        <input type="text" placeholder='Telefone' />
-
-        <button>Enviar</button>
-      </form>
+      <div className={styles.box}>
+        <form >
+          <span>Fale com a gente</span>
+          <input type="text" placeholder='Nome'/>
+          <input type="email" placeholder='E-mail' />
+          <input type="text" placeholder='Telefone' />
+          <button>Enviar</button>
+        </form>
+        <img src="peoples.svg" alt="" />
+      </div>
     </div>
   )
 }
